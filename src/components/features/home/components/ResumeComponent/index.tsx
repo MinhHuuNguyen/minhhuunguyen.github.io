7cf31e15a0fa2a1f9e723e896bbef5a6c4f610b3
TaiNguyen297 <tainguyen29702@gmail.com>
2024-05-23 21:11:08 +0700

    init project

--- src/components/features/home/components/ResumeComponent/index.tsx
+++ src/components/features/home/components/ResumeComponent/index.tsx
@@ -1,6 +1,8 @@
 import { Container, Box, Stack, Typography } from "@mui/material";
-import ktcbBackground from "../../../../../../public/mission-background.jpg";
+import ktcbBackground from "../../../../../../public/background-dep-4k.jpg";
 import LoadingButton from "@mui/lab/LoadingButton";
+import data from '../../../../../utils/data/json/resume.json';
+import { useRouter } from 'next/router';
 
 type ResumeProps = {
   missionCartData: {
@@ -10,70 +12,21 @@ type ResumeProps = {
   }[];
 };
 
-const resume = {
-  imgArea: {
-    imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO2IFYwOO5ZwC3KDAxzq2BWiyxjU905ZChbuAy1gKZtQ&s",
-
-  },
-
-  info: {
-    name: "Nguyen Huu Tai",
-  },
-
-  Experience: [
-    {
-      title: "Data Scientist - AI Engineer",
-      company: "PIXTA Vietnam Co. Ltd.",
-      duration: "May 2019 - Present (4 years 5 months)",
-
-    },
-
-    {
-      title: "Software Engineer",
-      company: "FPT Software",
-      duration: "May 2018 - May 2019 (1 year 1 month)",
-
-    },
-
-    {
-      title: "Software Engineer",
-      company: "FPT Software",
-      duration: "May 2018 - May 2019 (1 year 1 month)",
-
-    }
-    // Add more job experiences as needed
-  ],
-  Education: [
-    {
-      degree: "Master of Science",
-      field: "Mathematics and informatics",
-      school: "Hanoi University of Science and Technology",
-      duration: "Oct 2020 - Aug 2022"
-    },
-
-    {
-      degree: "Engineer's degree",
-      field: "Mathematics and informatics",
-      school: "Hanoi University of Science and Technology",
-      duration: "Aug 2015 - June 2020"
-    },
-    // Add more education experiences as needed
-  ],
-
-};
 
 export const ResumeComponent: React.FC<ResumeProps> = ({
 }) => {
+  const router = useRouter();
   return (
     <Container
-      sx={{
-        maxWidth: "1900px !important",
-        paddingTop: 7,
-        paddingBottom: 12,
-        backgroundImage: `url(${ktcbBackground.src})`,
-        backgroundSize: "100% 100%;",
-        backgroundPosition: "center",
-      }}
+    sx={{
+      maxWidth: "1900px !important",
+      paddingTop: 8,
+      paddingBottom: 10,
+      marginBottom: 5,
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${ktcbBackground.src})`,
+      backgroundSize: "100% 100%;",
+      backgroundPosition: "center",
+    }}
     >
       <Stack alignItems="center">
         <Typography
@@ -86,34 +39,53 @@ export const ResumeComponent: React.FC<ResumeProps> = ({
               sm: "2rem",
               md: "3rem",
             },
+
+            color: "white",
           }}
         >
           ABOUT ME
         </Typography>
 
         <Stack direction="row" justifyContent="space-between" spacing={20}>
-          <Box flexBasis="33%" p={3}>
-            <img src={resume.imgArea.imgSrc} alt="Profile" style={{ width: '100%', borderRadius: '50%', border: '2px solid black' }} />
-            <Typography variant="h6" style={{ textAlign: 'center' }}>{resume.info.name}</Typography>
+          <Box flexBasis="33%" p={2}>
+            <img src={data.header.imgArea.imgSrc} alt="Profile" style={{ width: '350px', height: '350px', borderRadius: '50%', border: '2px solid white' }} />
           </Box>
 
           <Box flexBasis="33%" p={2}>
-            {resume.Experience.map((job, index) => (
-              <Box key={index} style={{ margin: '10px 0', padding: '10px' }}>
-                <Typography variant="h6" style={{ fontWeight: 'bold' }}>{job.title}</Typography>
-                <Typography style={{ fontStyle: 'italic' }}>{job.company}</Typography>
-                <Typography>{job.duration}</Typography>
-              </Box>
-            ))}
+            {data.main.left.Experience.slice(0, 5).map((job: { title: string; company: string; startDate: string; endDate: string; }, index: number) => {
+              const startDate = new Date(job.startDate);
+              let endDate = new Date(); // Default to current date
+
+              if (job.endDate !== 'Present') {
+                endDate = new Date(job.endDate);
+              }
+
+              const diffTime = Math.abs(endDate.getTime() - startDate.getTime());
+              const diffYears = Math.floor(diffTime / (1000 * 60 * 60 * 24 * 365.25));
+              const diffMonths = Math.floor((diffTime % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * 30));
+
+              const yearString = diffYears > 1 ? `${diffYears} years` : diffYears === 1 ? `${diffYears} year` : '';
+              const monthString = diffMonths > 1 ? `${diffMonths} months` : diffMonths === 1 ? `${diffMonths} month` : '';
+
+              const duration = [yearString, monthString].filter(Boolean).join(', ');
+
+              return (
+                <Box key={index} style={{ margin: '10px 0', padding: '10px' }}>
+                  <Typography variant="h6" style={{ fontWeight: 'bold',color: 'white' }}>{job.title}</Typography>
+                  <Typography style={{ fontStyle: 'italic', color: 'white' }}>{job.company}</Typography>
+                  <Typography style={{ fontStyle: 'italic',color: 'white' }}>{job.startDate} - {job.endDate} ({duration})</Typography>
+                </Box>
+              );
+            })}
           </Box>
 
           <Box flexBasis="33%" p={2}>
-            {resume.Education.map((education, index) => (
+            {data.main.right.Education.map((education: { degree: string; field: string; school: string; duration: string; }, index: number) => (
               <Box key={index} style={{ margin: '10px 0', padding: '10px' }}>
-                <Typography variant="h6" style={{ fontWeight: 'bold' }}>{education.degree}</Typography>
-                <Typography style={{ fontStyle: 'italic' }}>{education.field}</Typography>
-                <Typography style={{ fontStyle: 'italic' }}>{education.school}</Typography>
-                <Typography>{education.duration}</Typography>
+                <Typography variant="h6" style={{ fontWeight: 'bold',color: 'white' }}>{education.degree}</Typography>
+                <Typography style={{ fontStyle: 'italic',color: 'white' }}>{education.field}</Typography>
+                <Typography style={{ fontStyle: 'italic',color: 'white' }}>{education.school}</Typography>
+                <Typography style={{ fontStyle: 'italic',color: 'white' }}>{education.duration}</Typography>
               </Box>
             ))}
           </Box>
@@ -121,10 +93,19 @@ export const ResumeComponent: React.FC<ResumeProps> = ({
 
         <LoadingButton
           variant="outlined"
+          onClick={() => router.push('/resume')}
           sx={{
             alignSelf: "center",
             width: "fit-content",
-          }}>
+            color: "white",
+            borderColor: "white",
+            "&:hover": {
+              borderColor: "red",
+              color: "red",
+              backgroundColor: "white",
+            },
+          }}
+        >
           Load More
         </LoadingButton>
       </Stack>
