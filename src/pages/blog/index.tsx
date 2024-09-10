6fae13e4cc6422c425e34cac7fbbbf332d519a11
MinhHuuNguyen <minhhuunguyen1511@gmail.com>
2024-09-10 17:44:45 +0700

    Fix to show posts from md file

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -15,14 +15,7 @@ export default function Blog(props: { posts: any }) {
     <>
       <DefaultSeo {...SEO} title={"Danh sách bài viết"} />
       <Stack>
-        <Box className="relative">
-          {/* <img
-        className="absolute top-1 left-1 w-12 h-12 object-cover z-10"
-        src={logoImg.src}
-        alt="banner"
-      /> */}
-          <CoverImageBrand />
-        </Box>
+        <CoverImageBrand />
         <Container maxWidth="xl">
           <Stack
             sx={{
@@ -51,29 +44,29 @@ export async function getStaticProps() {
   // Getting all our posts at build time
 
   // Get all the directories from the root directory
-  const root = 'ai-lectures'; // replace with your root directory
+  const root = 'ai-lectures/'; // replace with your root directory
   const dirs = fs.readdirSync(root).filter((file) => fs.statSync(path.join(root, file)).isDirectory());
 
   // Loop over each directory to get all the posts
-const posts = dirs.flatMap((dir) => {
-  // Get all the markdown files from the current directory
-  const files = fs.readdirSync(path.join(root, dir)).filter(file => file.endsWith('.md'));
+  const posts = dirs.flatMap((dir) => {
+    // Get all the markdown files from the current directory
+    const files = fs.readdirSync(path.join(root, dir)).filter(file => file.endsWith('.md'));
 
-  // Loop over each post to extract the frontmatter which we need
-  return files.map((file) => {
-    // getting the slug here which we will need as a URL query parameter
-    const slug = file.replace(".md", "");
-    // Reading the contents of the file
-    const filecontent = fs.readFileSync(path.join(root, dir, file), "utf-8");
-    const parsedContent = matter(filecontent);
-    //The parsed content contains data and content we only need the data which is the frontmatter
-    const { data } = parsedContent
-    return {
-      slug,
-      data,
-    };
+    // Loop over each post to extract the frontmatter which we need
+    return files.map((file) => {
+      // getting the slug here which we will need as a URL query parameter
+      const slug = file.replace(".md", "");
+      // Reading the contents of the file
+      const filecontent = fs.readFileSync(path.join(root, dir, file), "utf-8");
+      const parsedContent = matter(filecontent);
+      //The parsed content contains data and content we only need the data which is the frontmatter
+      const { data } = parsedContent
+      return {
+        slug,
+        data,
+      };
+    });
   });
-});
 
   return {
     props: {
