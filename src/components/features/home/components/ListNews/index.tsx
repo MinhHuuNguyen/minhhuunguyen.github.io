db5fa7b05cdbd91647bf0012741dbe6687ddada5
TaiNguyen297 <tainguyen29702@gmail.com>
2024-09-17 14:59:00 +0700

    cardNew in homepage

--- src/components/features/home/components/ListNews/index.tsx
+++ src/components/features/home/components/ListNews/index.tsx
@@ -1,66 +1,58 @@
-// /* eslint-disable @next/next/no-img-element */
-// import { INews } from "@/@types/news";
-// // import { getHighlightNews, getMediumNews } from "@/utils/common";
-// import { Container, Grid, Stack, Typography } from "@mui/material";
-// import { NextPage } from "next";
-// import { CardNews } from "./CardNews";
-// import { useMemo } from "react";
-// import ktcbBackground from "../../../../../../public/posts-background.jpg";
+import { Container, Grid, Stack, Typography } from "@mui/material";
+import { PostList } from "@/@types/post";
+import { CardNews } from "./CardNews"; // Import component CardNews
+import ktcbBackground from "../../../../../../public/posts-background.jpg"; // Đường dẫn tới ảnh nền
 
-// interface Props {
-//   team: string;
-// }
+interface Props {
+  highlightPosts: PostList[];
+}
 
-// const ListNewsHome: NextPage<Props> = ({ team }) => {
-//   const highlightNews = getHighlightNews(team);
-//   const mediumNews = getMediumNews(team);
-//   const listNew = useMemo(() => {
-//     return [highlightNews, ...mediumNews.slice(0, 5)].filter((x) => x);
-//   }, [highlightNews, mediumNews]);
+const ListNewsHome = ({ highlightPosts}: Props) => {
+  const listNew = [...highlightPosts];
 
-//   return listNew && listNew?.length > 0 ? (
-//     <Container
-//       sx={{
-//         maxWidth: "1900px !important",
-//         paddingTop: 0,
-//         backgroundImage: `url(${ktcbBackground.src})`,
-//         backgroundSize: "100% 100%;",
-//         backgroundPosition: "center",
-//       }}
-//     >
-//       <Stack alignItems="center">
-//         <Typography
-//           variant="h3"
-//           fontWeight="bold"
-//           textAlign="center"
-//           sx={{
-//             fontSize: {
-//               xs: "1.5rem",
-//               sm: "2rem",
-//               md: "3rem",
-//             },
-//           }}
-//         >
-//           TIN TỨC GẦN ĐÂY
-//         </Typography>
-//       </Stack>
-//       <Stack
-//         sx={{
-//           paddingTop: "60px",
-//           paddingBottom: "60px",
-//           gap: "30px",
-//         }}
-//       >
-//         <Grid container spacing={2}>
-//         {listNew.map((post: any, index: any) => (
-//             <Grid item xs={12} md={6} lg={4} key={index}>
-//                 <CardNews key={index} post={post} />
-//             </Grid>
-//         ))}
-//         </Grid>
-//       </Stack>
-//     </Container>
-//   ) : null;
-// };
+  return(
+    <Container
+      sx={{
+        maxWidth: "1900px !important",
+        paddingTop: 0,
+        backgroundImage: `url(${ktcbBackground.src})`,
+        backgroundSize: "100% 100%;",
+        backgroundPosition: "center",
+      }}
+    >
+      <Stack alignItems="center">
+        <Typography
+          variant="h3"
+          fontWeight="bold"
+          textAlign="center"
+          sx={{
+            fontSize: {
+              xs: "1.5rem",
+              sm: "2rem",
+              md: "3rem",
+            },
+          }}
+        >
+          FEATURED NEWS
+        </Typography>
+      </Stack>
+      <Stack
+        sx={{
+          paddingTop: "60px",
+          paddingBottom: "60px",
+          gap: "30px",
+        }}
+      >
+        <Grid container spacing={2}>
+          {listNew.map((post, index) => (
+            <Grid item xs={12} md={6} lg={4} key={index}>
+              <CardNews key={index} post={post} />
+            </Grid>
+          ))}
+        </Grid>
+      </Stack>
+    </Container>
+  );
+};
 
-// export default ListNewsHome;
+export default ListNewsHome;
