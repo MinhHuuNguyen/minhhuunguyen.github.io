3f2455ef7d350f738b76da14ee9cd69d9e1d291b
MinhHuuNguyen <minhhuunguyen1511@gmail.com>
2024-10-11 21:02:57 +0700

    Add data.is_published field

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -22,17 +22,19 @@ export function getPostsList(): { highlightPosts: PostList[], nonHighlightPosts:
         const { data } = parsedContent;
         const slug = data.slug; // Lấy slug từ markdown
 
-        postList.push({
-          time: data.time || null,
-          title: data.title || null,
-          description: data.description || null,
-          author: data.author || null,
-          banner_url: data.banner_url,
-          tags: data.tags || null,
-          slug,
-          filePath: fullPath,
-          isHighlight: data.is_highlight || false, 
-        });
+        if (data.is_published === true) {
+          postList.push({
+            time: data.time || null,
+            title: data.title || null,
+            description: data.description || null,
+            author: data.author || null,
+            banner_url: data.banner_url,
+            tags: data.tags || null,
+            slug,
+            filePath: fullPath,
+            isHighlight: data.is_highlight || false, 
+          });
+        }
       }
     }
   };
