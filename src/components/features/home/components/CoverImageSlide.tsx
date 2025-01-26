8c2f478450a8e2620b536a1d74c49f29baabd994
MinhHuuNguyen <minhhuunguyen1511@gmail.com>
2025-01-26 15:39:19 +0700

    Add image slide data

--- src/components/features/home/components/CoverImageSlide.tsx
+++ src/components/features/home/components/CoverImageSlide.tsx
@@ -51,14 +51,8 @@ export const CoverImageSlide: React.FC<CoverImageSlideType> = ({
   coverImageData,
 }) => {
   const listImageCard = coverImageData.map((url, index) => (
-    <Box className="relative w-full h-[90vh]" key={`${index}${url}`}>
+    <Box className="relative w-full h-[85vh]" key={`${index}${url}`}>
       <Box component="img" width="100%" height="100%" src={url} />
-
-      {/* <img
-        className="absolute top-1 left-1 w-12 h-12 object-cover z-10"
-        src={logoImg.src}
-        alt="banner"
-      /> */}
     </Box>
   ));
 
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,6 +1,6 @@
 import LanguageIcon from "@mui/icons-material/Language";
 import MenuIcon from "@mui/icons-material/Menu";
-import { Box, IconButton, Button, Paper, Stack, Typography } from "@mui/material";
+import { Box, IconButton, Stack } from "@mui/material";
 import Drawer from "@mui/material/Drawer";
 import Link from "next/link";
 import menuData from "../../utils/data/header.json";
@@ -10,7 +10,6 @@ import MenuSection from "./Menu";
 import { useState } from "react";
 import VerticalMenu from "./Menu/SideMenu/SideMenuSection";
 import Image from "next/image";
-// import { COLORS } from "@/utils/constants";
 
 export type MenuType = typeof menuData;
 
@@ -23,23 +22,16 @@ const Header = () => {
 
   return (
     <Box
-      sx={{
-        boxShadow: "rgba(0, 0, 0, 0.08) 0px 4px 15px",
-        px: {
-          xs: 1,
-          md: 2,
-        },
-      }}
+      sx={{ boxShadow: "rgba(0, 0, 0, 0.08) 0px 4px 15px" }}
     >
       <Stack
         direction="row"
         justifyContent="space-between"
         alignItems="center"
-        my={0}
         sx={{
-          py: 2,
+          py: 1,
           maxWidth: "1400px",
-          margin: "0 auto",
+          margin: "auto",
         }}
       >
         <IconButton
@@ -49,11 +41,7 @@ const Header = () => {
           <MenuIcon sx={{ fontSize: 30 }} />
         </IconButton>
         <Link href="/" className="flex items-center">
-          <Image
-            src={logoRectangle.src}
-            alt="logoRectangle"
-            width={70}
-            height={70}
+          <Image src={logoRectangle.src} alt="logoRectangle" width={50} height={50}
           />
         </Link>
         <Box display={["none", "none", "none", "block"]}>
@@ -61,18 +49,16 @@ const Header = () => {
             direction="row"
             justifyContent="center"
             alignItems="center"
-            spacing={7}
+            spacing={10}
           >
             {menuData.map((menu, index) => (
               <MenuSection key={`${index}menu`} menuData={menu} />
             ))}
-
           </Stack>
-
         </Box>
 
         <IconButton>
-          <LanguageIcon sx={{ fontSize: 25 }} />
+          <LanguageIcon sx={{ fontSize: 20 }} />
         </IconButton>
       </Stack>
 
