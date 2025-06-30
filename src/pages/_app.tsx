59d17309dd3cd48c0271cecb81e1b430a723a26f
MinhHuuNguyen <minhhuunguyen1511@gmail.com>
2025-06-30 13:07:38 +0700

    Fix googletagmanager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,6 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { CssBaseline } from "@mui/material";
 import { useEffect, useState } from "react";
-import Script from "next/script";
 
 const clientSideEmotionCache = createEmotionCache();
 export interface MyAppProps extends AppProps {
@@ -30,20 +29,6 @@ export default function App(props: MyAppProps) {
 
   return (
     <CacheProvider value={emotionCache}>
-      <Script
-        id="googletagmanager"
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-
-      <Script id="next_public_google_analytics" strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-         `}
-      </Script>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,13 +19,13 @@ export default function Document() {
           rel="stylesheet"
         />
 
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-E7NY2W59JZ"/>
+        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
         <Script id="google-analytics">
           {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', ${'${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'});
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
           `}
         </Script>
       </Head>
