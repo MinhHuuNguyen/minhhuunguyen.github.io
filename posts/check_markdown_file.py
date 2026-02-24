bde813148eb3cb581555d2e08459f04fa5da384c
MinhHuuNguyen <minhhuunguyen1511@gmail.com>
2026-02-24 16:00:57 +0700

    Update submodule

--- posts/check_markdown_file.py
+++ posts/check_markdown_file.py
@@ -8,56 +8,39 @@ def read_markdown_files(directory):
     
     for root, _, files in os.walk(directory):
         for file in files:
-            if file.endswith(".md") and file not in ["README.md", "LICENSE.md", "AUTHORS.md", "ICON_LICENSE.md", "COPYING.md"]:
-                file_path = os.path.join(root, file)
-                with open(file_path, "r", encoding="utf-8") as f:
-                    post = frontmatter.loads(f.read())
-                    metadata = post.metadata
-                    metadata["file_path"] = file_path.split('/')[-1]
-                    markdown_data.append(metadata)
+            if not file.endswith(".md"): continue
+            if "_venv/.venv/" in root: continue
+            if file in ["README.md"]: continue
+            # print(root, file)
+            file_path = os.path.join(root, file)
+            with open(file_path, "r", encoding="utf-8") as f:
+                post = frontmatter.loads(f.read())
+                metadata = post.metadata
+                metadata["file_name"] = file_path.split('/')[-1]
+                metadata["file_path"] = file_path
+                markdown_data.append(metadata)
     
     return pd.DataFrame(markdown_data)  # Convert to pandas DataFrame
 
 # Example usage:
-directory_path = "../posts"  # Change this to your target directory
+directory_path = "."  # Change this to your target directory
 md_df = read_markdown_files(directory_path)
-md_df = md_df[['time', 'file_path', 'title']]
+md_df = md_df[['time', 'file_name', 'is_published', 'title', 'file_path']]
 md_df['time'] = pd.to_datetime(md_df['time'])
 md_df = md_df.sort_values(by='time', ascending=True)
 md_df = md_df.reset_index(drop=True)
 
+# Print dataframe
 print(md_df)
-print(md_df.columns)
+
+# Print rows whose time is same
+duplicate_times = md_df[md_df.duplicated(subset=['time'], keep=False)]
+print("Duplicate time entries:")
+print(duplicate_times)
+
 md_df.to_csv('posts.csv')
 
 
-# 2022-11-11
-# 2022-11-14
-# 2022-11-18
-# 2022-11-21
-# 2022-11-28
-# 2022-12-02
-# 2022-12-05
-# 2022-12-09
-# 2022-12-12
-
-# 01/10/2023
-
-# 12/05/2023
-# 11/24/2023
-# 12/08/2023
-# 12/12/2023
-# 12/15/2023
-
-
-# 20230405 - Buổi 6
-# 20230408
-# 20230410 - Buổi 7
-# 20230414 - Buổi 8
-# 20230417 - Buổi 9
-# 20230424 - Buổi 10
-# 20230428 - Buổi 11
-# [DWDL23A4L1] 20230503 - Buổi 2
 # 20230506
 # [DWDL23A4L1] 20230508 - Buổi 3
 # 20230513
@@ -93,3 +76,8 @@ md_df.to_csv('posts.csv')
 # [PROTONX-1] 20231226 - Buổi 9
 # [PROTONX-1] 20231219 - Buổi 8
 
+# 01/10/2023
+# 12/05/2023
+# 12/08/2023
+# 12/12/2023
+# 12/15/2023
